617bfd36ae57dd84af8669e54e203dec76debdee
miv-dev <i@mike39.ru>
2023-09-17 22:14:47 +0300

    Add database libraries and DBConfig

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
-
+val hikari_version: String by project
 val exposed_version: String by project
-val h2_version: String by project
 val postgres_version: String by project
 plugins {
     kotlin("jvm") version "1.9.10"
@@ -26,15 +25,22 @@ repositories {
 }
 
 dependencies {
+
+    implementation("org.jsoup:jsoup:1.15.3")
+
+    implementation("org.postgresql:postgresql:$postgres_version")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+    implementation("com.zaxxer:HikariCP:$hikari_version")
+
+
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-host-common-jvm")
     implementation("io.ktor:ktor-server-call-logging-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("com.h2database:h2:$h2_version")
-    implementation("org.postgresql:postgresql:$postgres_version")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
