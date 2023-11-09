24b9f007fab4865262f0b21662a0d9ce7ecb1839
miv-dev <i@mike39.ru>
2023-11-09 17:03:17 +0300

    Fix bug with path

--- src/main/kotlin/ru/dev/miv/routing/ProgramRouting.kt
+++ src/main/kotlin/ru/dev/miv/routing/ProgramRouting.kt
@@ -5,6 +5,7 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import ru.dev.miv.response_models.ParsingRequest
 import ru.dev.miv.services.ProgramService
 
 
@@ -14,10 +15,16 @@ fun Route.programRouting() {
         
 
         post("/parsing") {
-            val html = call.receiveText()
-            val program = service.parsing(html)
+            val parsingRequest = call.receive<ParsingRequest>()
+            runCatching {
+                service.parsing(parsingRequest)
+            }.onSuccess {
+                call.respond(it)
+            }.onFailure {
+                call.respond(HttpStatusCode.BadRequest, hashMapOf("error" to it.localizedMessage ))
+            }
 
-            call.respond(program)
+
         }
 
         post {
